ff41981f9b1420692b56577333f594511b21ab2d
jeelparmar123 <85307513+jeelparmar123@users.noreply.github.com>
2023-01-16 18:32:43 +0530

    Update Single_Integer.cpp

--- Single_Integer.cpp
+++ Single_Integer.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
  
-int findSingle(int ar[], int ar_size)
+int solve(int arr[], int ar_size)
 {
     // Do XOR of all elements and return
-    int res = ar[0];
+    //same solution we can do it in for loop manner but it will go into the o(n^2) time complexity but XOR will do in a o(n).
+    int ans = arr[0];
     for (int i = 1; i < ar_size; i++)
-        res = res ^ ar[i];    //by doing OR identical elements will gets cancel out
+        ans = ans ^ arr[i];    //by doing OR identical elements will gets cancel out
  
-    return res;
+    return ans;          //solution works in O(n) time.
 }
  
 int main()
@@ -17,12 +18,12 @@ int main()
     int size=0;
     cout<<"Enter the size of the array:";
     cin>>size;
-    int ar[size];
+    int arr[size];
     cout<<"\nEnter array:\n";
     for(int i=0; i<size; i++){
         cout<<"Insert element "<<i+1<<": ";
-        cin>>ar[i];
+        cin>>arr[i];
     }
-    cout << "Element occurring once is "<< findSingle(ar, size);
+    cout << "Element occurring once is "<< solve(arr, size);
     return 0;
 }
